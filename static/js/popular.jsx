77a92ca223b98a50ff33a4524093323f60591b5f
Katie Byers <lobsterkatie@gmail.com>
2018-04-16 18:12:28 -0700

    factored language selector out into its own component

--- static/js/popular.jsx
+++ static/js/popular.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import LanguageSelector from "./language-selector.jsx";
 
 export default class Popular extends React.Component {
     constructor(props) {
@@ -22,46 +23,14 @@ export default class Popular extends React.Component {
     }
 
     render() {
-        let languages = ["All", "JavaScript", "Ruby", "Java", "CSS", "Python"];
 
         return (
-            <ul className="languages">
-
-                {languages.map(function(language){
-
-                    return (
-                        <li
-                          style={ language === this.state.selectedLanguage ?
-                                    {color: "#D0021B"} :
-                                    null } {/* ternary: <if cond> ? <body of
-                                               if> : <body of else> */}
-
-                          key={language}
-                          {/* Whenever you do map to create an array of DOM
-                              elements, you need to give each one a unique
-                              key property. */}
-
-                          onClick={this.updateLanguage.bind(null, language)}
-                          {/* We need to bind here to pass in the language
-                            argument. The first argument to bind is null
-                            because we bound the correct 'this' (the
-                            component) above, in the constructor. */}
-
-                        > {/* end of opening <li> tag */}
-
-                            {language}
-
-                        </li>
-                    )
-                }, this)
-                {/* The 'this' here is to make the 'this' inside the map
-                callback the same as the 'this' outside the callback (ie,
-                    to make 'this' mean the component). If we use arrow
-                functions, we don't need to do this. */}
-
-                } {/* end of array of <li>'s */}
-
-            </ul>
+            <div>
+                <LanguageSelector
+                  selectedLanguage={this.state.selectedLanguage}
+                  onSelect={this.updateLanguage}
+                />
+            </div>
         )
     } //end render()
 
